712403d35663ae603391aaa1e150dc328690cba5
Ankit Sarraf <sarrafankit@hotmail.com>
2015-05-31 23:58:42 -0700

    Code Cleaning of App.java

--- MavenSql/src/main/java/com/cloudwick/maven/MavenSql/App.java
+++ MavenSql/src/main/java/com/cloudwick/maven/MavenSql/App.java
@@ -11,27 +11,20 @@ import java.sql.Statement;
 import java.util.Properties;
 
 /**
- * Hello world!
- *
+ * This file base code has been taken from 
+ * http://www.tutorialspoint.com/jdbc/jdbc-sample-code.htm
  */
 public class App {
-	// static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-	// static final String DB_URL = "jdbc:mysql://172.17.1.113:3306/cloudwick1";
-	// static final String DB_URL = "jdbc:mysql://localhost:8990/cloudwick";
-
-	// Database credentials
-	// static final String USER = "root";
-	// static final String PASS = "ankit";
-	// static final String PASS = "root";
-
 	public static void main(String[] args) throws IOException,
 			ClassNotFoundException, SQLException {
 
+		//STEP 1 : Extract the Connection data from db.properties file
 		Properties props = new Properties();
 		InputStream in = new FileInputStream("target/classes/db.properties");
 		props.load(in);
 		in.close();
 
+		// STEP 2 : Establish connection with the Database
 		String driver = props.getProperty("db.driver");
 		if (driver != null) {
 			System.out.println(driver);
@@ -48,34 +41,31 @@ public class App {
 		System.out.println("Connecting to database...");
 		conn = DriverManager.getConnection(url, username, password);
 
-		// STEP 4: Execute a query
+		// STEP 3: Execute a query
 		System.out.println("Creating statement...");
 		stmt = conn.createStatement();
 
 		String sql = "SELECT id, ename FROM emp";
 		ResultSet rs = stmt.executeQuery(sql);
 
-		// STEP 5: Extract data from result set
+		// STEP 4: Extract data from result set
 		while (rs.next()) {
 			// Retrieve by column name
 			int id = rs.getInt("id");
 			String ename = rs.getString("ename");
 
-			// Display values
+			// STEP 5 : Display values
 			System.out.print("Id: " + id);
 			System.out.println(", Name: " + ename);
 		}
+		
 		// STEP 6: Clean-up environment
-		rs.close();
-		stmt.close();
-		conn.close();
-
-		// Remove resources
 		if (stmt != null)
 			stmt.close();
 		if (conn != null)
 			conn.close();
-
-		System.out.println("Goodbye!");
+		if (rs != null) {
+			rs.close();
+		}
 	}
 }
